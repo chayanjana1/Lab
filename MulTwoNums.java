1c1aac0aace706009884fa8d921641b388fbd6a8
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-06-23 14:07:50 +0530

    Add files via upload

---
+++ MulTwoNums.java
@@ -0,0 +1,12 @@
+//program is multiply two value not taking user.
+class MulTwoNums
+{
+// Main Class
+	public static void main(String args[]){
+		int a=59;// Giving a value
+		int b=23;// Giving b value
+		System.out.println(a*b);
+}
+}	
+
+		
