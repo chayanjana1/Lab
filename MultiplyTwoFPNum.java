c9ea802f6518761260e791d054a2e49e8eae1c5b
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-06-23 14:12:14 +0530

    Add files via upload

---
+++ MultiplyTwoFPNum.java
@@ -0,0 +1,13 @@
+//This program is two Floating value multiplication..
+public class MultiplyTwoFPNum{
+	//Main class
+    public static void main(String[] args) {
+
+        float first = 1.5f; // Given value.
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
