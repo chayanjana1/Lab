abd35daf9b0e2bd68764a589b7a2f182b104a65e
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-06-23 14:06:32 +0530

    Add files via upload

---
+++ AddTwoNums.java
@@ -0,0 +1,12 @@
+// This program is add two value not taking from user.
+class AddTwoNums
+{
+     //Main Class	
+	public static void main(String args[]){
+		int a=45;//Giving a value
+		int b=40;//Giving b value
+		System.out.println(a+b);
+}
+}	
+
+		
